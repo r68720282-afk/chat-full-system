b59b132341c737bd8690a90542540b11b847d221
r68720282-afk <r68720282@gmail.com>
2025-11-25 23:47:06 +0530

    Update logs.js

--- server/modules/logs.js
+++ server/modules/logs.js
@@ -1,1 +1,12 @@
 
+// server/modules/logs.js
+let ACTIVITY_LOGS = [];
+
+function addLog(type, data){
+  ACTIVITY_LOGS.push({ id: Date.now()+"_"+Math.random().toString(36).slice(2,8), type, data, time: Date.now() });
+  if (ACTIVITY_LOGS.length > 5000) ACTIVITY_LOGS.shift();
+}
+function getLogs(limit=200){
+  return ACTIVITY_LOGS.slice(-limit).reverse();
+}
+module.exports = { addLog, getLogs };
