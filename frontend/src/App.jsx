ff2147c92fd715cad6573c544128afce41bddf97
r68720282-afk <r68720282@gmail.com>
2025-12-01 01:39:38 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,18 @@
-// file placeholder
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to ChatSite</h1>
+
+      <p>Select an option:</p>
+
+      <ul style={{ lineHeight: "2rem" }}>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/rooms">Chat Rooms</Link></li>
+      </ul>
+    </div>
+  );
+}
