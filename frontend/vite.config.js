638960d85bd9a963d291723cab3cfb41726eb857
r68720282-afk <r68720282@gmail.com>
2025-12-02 00:30:03 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // <-- सबसे ज़रूरी लाइन!
   plugins: [react()],
   server: {
     proxy: {
